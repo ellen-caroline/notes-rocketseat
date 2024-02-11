e52642fce761e141c74bb4926f38958c77a3e7f4
Ellen <ellenc.contato@gmail.com>
2024-02-11 18:17:52 -0300

    feat: :iphone: add responsiveness into grid, new note and note

--- src/app.tsx
+++ src/app.tsx
@@ -47,8 +47,8 @@ export function App() {
     : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={ logo } alt="Notes" width={100} />
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+      <img src={ logo } alt="Notes" width={ 100 } />
 
       <form className="w-full">
         <input 
@@ -61,7 +61,7 @@ export function App() {
 
       <div className="h-px bg-zinc-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map(note => {
           return <NoteCard key={note.id} note={note}/>
