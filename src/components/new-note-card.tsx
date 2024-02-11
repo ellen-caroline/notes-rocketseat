3376d6e178fdb7c9ce193c3a77c181d99c742163
Ellen <ellenc.contato@gmail.com>
2024-02-11 02:23:05 -0300

    feat: :sparkles: add event handler to text area note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,20 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
+import { ChangeEvent, useState } from 'react';
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    if(event.target.value === '') {
+      setShouldShowOnboarding(true)
+    }
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-zinc-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-zinc-600 focus-visible:ring-2 focus-visible:ring-l1lac">
@@ -24,9 +37,18 @@ export function NewNoteCard() {
             <span className="text-sm font-medium text-zinc-300">
               Adicionar nota
             </span>
-            <p className="text-sm leading-6 text-zinc-400">
-              Comece <button className="font-md text-purple-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button className="font-md text-purple-400 hover:underline">utilize apenas texto</button>.
-            </p>
+            {shouldShowOnboarding ? (
+              <p className="text-sm leading-6 text-zinc-400">
+                Comece <button className="font-md text-purple-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-md text-purple-400 hover:underline">utilize apenas texto</button>.
+              </p>
+            ) : (
+              <textarea 
+                autoFocus 
+                className="text-sm leading-6 text-zinc-400 bg-transparent resize-none flex-1 outline-none"
+                onChange={handleContentChanged}
+              />
+            )} 
+            
           </div>
 
           <button 
