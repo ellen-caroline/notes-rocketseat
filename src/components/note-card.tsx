5ff3faf73c26f434be9327cf259b69231169f816
Ellen <ellenc.contato@gmail.com>
2024-02-11 00:46:46 -0300

    build: :arrow_up: install radix UI dialog

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,12 +1,20 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-zinc-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-zinc-600 focus-visible:ring-2 focus-visible:ring-l1lac">
+    <button className="rounded-md text-left flex flex-col bg-zinc-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-zinc-600 focus-visible:ring-2 focus-visible:ring-l1lac">
       <span className="text-sm font-medium text-zinc-300">
-        Há 2 dias
+        {note.date.toISOString()}
        </span>
       <p className="text-sm leading-6 text-zinc-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores placeat deserunt tenetur non, neque amet dicta! Facere est voluptate nobis a rem error expedita, nesciunt deleniti eveniet quod repellat corrupti!
+        {note.content}
       </p>
+
        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-zinc-900/60 to-zinc-900/0 pointer-events-none" />
     </button>
   )
